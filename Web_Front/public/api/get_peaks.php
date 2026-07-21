ce3136f88a0d00dcd2a358f60511c1a4d3452fe2
Anthony Kuzub <4483@apkaudio.com>
2026-07-20 23:04:53 -0400

    Remove unbuffered query and limit

--- Web_Front/public/api/get_peaks.php
+++ Web_Front/public/api/get_peaks.php
@@ -13,7 +13,6 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false,
 ];
 
 try {
@@ -41,7 +40,6 @@ try {
         LEFT JOIN spectral_features s ON a.id = s.file_id
         LEFT JOIN musicality mu ON a.id = mu.file_id
         LEFT JOIN envelope e ON a.id = e.file_id
-        LIMIT 10
     ";
 
     $stmt = $pdo->query($sql);
